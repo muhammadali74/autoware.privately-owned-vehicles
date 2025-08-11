b620fd9d605ca361663f202b340f0dff08ce5894
Pranav Doma <pranavreddy2327@gmail.com>
2025-08-11 08:19:23 -0500

    inference flag header
    Signed-off-by: Pranav Doma <pranavreddy2327@gmail.com>

--- VisionPilot/ROS2/models/include/run_model_node.hpp
+++ VisionPilot/ROS2/models/include/run_model_node.hpp
@@ -5,6 +5,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <image_transport/image_transport.hpp>
 #include <memory>
+#include <chrono>
 
 namespace autoware_pov::vision
 {
@@ -27,6 +28,12 @@ private:
   // ROS
   image_transport::Subscriber sub_;
   image_transport::Publisher pub_;
+  
+  // Latency monitoring (like original AUTOSEG)
+  static constexpr size_t LATENCY_SAMPLE_INTERVAL = 100; // Log every 100 frames
+  size_t frame_count_ = 0;
+  bool measure_latency_ = false;
+  std::chrono::steady_clock::time_point inference_start_time_;
 };
 
 }  // namespace autoware_pov::vision
