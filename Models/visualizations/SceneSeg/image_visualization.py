f49df3ca73ddcf31cfa0e24e448d4424f80aa216
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-08-14 16:09:14 +0300

    visualizing freespace mask separately

--- Models/visualizations/SceneSeg/image_visualization.py
+++ Models/visualizations/SceneSeg/image_visualization.py
@@ -23,19 +23,16 @@ def make_visualization_freespace(prediction, image):
   # Getting freespace object labels
   free_space_labels = np.where(prediction == 2)
 
+  # Getting shape
   shape = prediction.shape
   row = shape[0]
   col = shape[1]
+
+  # Creating visualization
   binary_mask = np.zeros((row, col), dtype = "uint8")
   binary_mask[free_space_labels[0], free_space_labels[1]] = 255
   edge_contour = find_freespace_edge(binary_mask)
   cv2.fillPoly(colour_mask, pts =[edge_contour], color=(28,255,145))
-  #cv2.drawContours(colour_mask, edge_contour, -1, color=(28, 255, 145), thickness=-1)
-
-  # Assigning freespace objects colour
-  #colour_mask[free_space_labels[0], free_space_labels[1], 0] = 28
-  #colour_mask[free_space_labels[0], free_space_labels[1], 1] = 255
-  #colour_mask[free_space_labels[0], free_space_labels[1], 2] = 145
 
   # Converting to OpenCV BGR color channel ordering
   colour_mask = cv2.cvtColor(colour_mask, cv2.COLOR_RGB2BGR)
@@ -91,13 +88,16 @@ def main():
     # Run inference and create visualization
     print('Running Inference and Creating Visualization')
     prediction = model.inference(image_pil)
-    #vis_obj = make_visualization(prediction)
-    vis_obj = make_visualization_freespace(prediction, image_pil)
+    vis_obj = make_visualization(prediction)
+    vis_obj_freespace = make_visualization_freespace(prediction, image_pil)
 
     # Resize and display visualization
     vis_obj = cv2.resize(vis_obj, (frame.shape[1], frame.shape[0]))
+    vis_obj_freespace = cv2.resize(vis_obj_freespace, (frame.shape[1], frame.shape[0]))
     image_vis_obj = cv2.addWeighted(vis_obj, alpha, frame, 1 - alpha, 0)
+    image_vis_freespace = cv2.addWeighted(vis_obj_freespace, alpha, frame, 1 - alpha, 0)
     cv2.imshow('Prediction Objects', image_vis_obj)
+    cv2.imshow('Prediction Freespace', image_vis_obj)
     cv2.waitKey(0)
 
 if __name__ == '__main__':
