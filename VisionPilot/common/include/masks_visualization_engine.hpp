b7dbceefcc50bbd6bbeac682b3b99d5127613762
Pranav Doma <pranavreddy2327@gmail.com>
2025-08-17 21:15:21 -0500

    modified viusalize function
    Signed-off-by: Pranav Doma <pranavreddy2327@gmail.com>

--- VisionPilot/common/include/masks_visualization_engine.hpp
+++ VisionPilot/common/include/masks_visualization_engine.hpp
@@ -5,20 +5,24 @@
 #include <vector>
 #include <string>
 
+#ifdef CUDA_FOUND
+#include "cuda_visualization_kernels.hpp"
+#endif
+
 namespace autoware_pov::common {
 
 class MasksVisualizationEngine {
 public:
     explicit MasksVisualizationEngine(const std::string& viz_type);
     
-    // Core visualization logic - framework agnostic
+    // Simple mask visualization (clean approach)
     cv::Mat visualize(const cv::Mat& mask, const cv::Mat& original_image);
-    
+
 private:
-    void createColorMap(const std::string& viz_type);
     
     std::vector<cv::Vec3b> color_map_;
     std::string viz_type_;
+    bool use_cuda_;
 };
 
 } // namespace autoware_pov::common
